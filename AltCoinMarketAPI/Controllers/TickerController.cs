cf04031c20a9b21abcceb13aa70cd600b4b49852
adrianionita00 <adrianionita00@gmail.com>
2017-08-05 15:42:26 +0300

    Renaming files. Adding first WebAPI stubs.

---
+++ AltCoinMarketAPI/Controllers/TickerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AltCoinMarketAPI.Models;
+using AltCoinMarketConsumerLibrary;
+
+namespace AltCoinMarketAPI.Controllers
+{
+    public class TickerController : ApiController
+    {
+        private Ticker ticker;
+        //Ticker[] tickers = new Ticker[]
+        //{
+        //new Ticker { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+        //new Ticker { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+        //new Ticker { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        //};
+
+        //public IEnumerable<Ticker> GetAllTickers()
+        //{
+        //    return tickers;
+        //}
+
+        public IHttpActionResult GetTicker()
+        {
+            //var ticker = tickers.FirstOrDefault();
+            //if (tickers == null)
+            //{
+            //    return NotFound();
+            //}
+            //return Ok(ticker);
+
+            KrakenConsumer consumer = new KrakenConsumer();
+            var ticker = consumer.GetTicker(new List<string> { Constants.CurrencyPairs.BTCUSD });
+
+            if (ticker == null)
+            {
+                return NotFound();
+            }
+            return Ok(ticker);
+        }
+    }
+}
---
+++ AltCoinMarketAPI/Models/Ticker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AltCoinMarketConsumerLibrary;
+
+namespace AltCoinMarketAPI.Models
+{
+    public class Ticker
+    {
+        public List<object> error { get; set; }
+        public string currencyPair { get; set; }
+
+        public List<string> a { get; set; }
+        public List<string> b { get; set; }
+        public List<string> c { get; set; }
+        public List<string> v { get; set; }
+        public List<string> p { get; set; }
+        public List<int> t { get; set; }
+        public List<string> l { get; set; }
+        public List<string> h { get; set; }
+        public string o { get; set; }
+    }   
+
+    //<pair_name> = pair name
+    //a = ask array(<price>, <whole lot volume>, <lot volume>),
+    //b = bid array(<price>, <whole lot volume>, <lot volume>),
+    //c = last trade closed array(<price>, <lot volume>),
+    //v = volume array(<today>, <last 24 hours>),
+    //p = volume weighted average price array(<today>, <last 24 hours>),
+    //t = number of trades array(<today>, <last 24 hours>),
+    //l = low array(<today>, <last 24 hours>),
+    //h = high array(<today>, <last 24 hours>),
+    //o = today's opening price
+}
