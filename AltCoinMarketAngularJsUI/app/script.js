8688c0d4b53fc051e57b4645a1d6f432e578d3d8
adrianionita00 <adrianionita00@gmail.com>
2017-08-07 08:41:41 +0300

    Added the angular project
    Just a simple rest query for now :)

---
+++ AltCoinMarketAngularJsUI/app/script.js
@@ -0,0 +1,27 @@
+﻿angular.module('myapp', ['myapp.controller']);
+
+angular.module('myapp.controller', ['myapp.service'])
+    .controller('testController', function ($scope, testService) {
+
+        $scope.posts = {};
+        function GetAllPosts() {
+            var getPostsData = testService.getPosts();
+
+            getPostsData.then(function (post) {
+                $scope.posts = post.data;
+
+            }, function () {
+                alert('Error in getting post records');
+            });
+        }
+
+        GetAllPosts();
+    });
+
+angular.module('myapp.service', [])
+    .service('testService', function ($http) {
+        
+        this.getPosts = function () {            
+            return $http.get('http://localhost:52974/api/ticker');
+        };
+    });
